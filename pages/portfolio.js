12101fc367d9726ee1b03957944c81ee6139202d
PvrkFvmily <wlsgnl9973@gmail.com>
2023-03-02 14:40:30 -0800

    added github links to projects

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 export default function Portfolio() {
     return (
@@ -13,6 +14,7 @@ export default function Portfolio() {
                 <div>
                     <p>Created a Full-stack application using Express, React, Node.js and PostgreSQL. Made it look like Instagram. We incorporated Machine learning software called TensorFlow to detect if the sentence is hurtful and toxic. I was in charge of making users be able to post pictures and captions using cloudinaryAPI. Created a basic framework for backend routes.</p>
                 </div>
+                <Link target="_blank" href="https://github.com/tbfrobinson/eciN-Stagram-client"><h3>Check it out on GitHub!</h3></Link>
             </div> 
             <div className='project-item'>
                 <h1>McFacebook</h1>
@@ -22,6 +24,7 @@ export default function Portfolio() {
                 <div>
                     <p>Created a Full-stack application using MongoDB, Express, React, and Node.js in a group of 4. Made it look like Facebook. I was in charge of making the homepage and posts detail to conditionally render the edit button. Backend routes to the things that I worked on.</p>
                 </div>
+                <Link target="_blank" href="https://github.com/PvrkFvmily/domino-client"><h3>Check it out on GitHub!</h3></Link>
             </div>
             <div className='project-item'>
                 <h1>ACvillagercritic</h1>
@@ -31,6 +34,7 @@ export default function Portfolio() {
                 <div>
                     <p>Created a website using Animal Crossing api. I was in charge of creating a full stack application that is deployed on the internet. Using acnhapi's document to get information of villagers and saving that data in the database. Users being able to make accounts and favoriting and comment on their favorite villagers.</p>
                 </div>
+                <Link target="_blank" href="https://github.com/PvrkFvmily/project-2-ACvillagercritic"><h3>Check it out on GitHub!</h3></Link>
             </div>
             <div className='project-item'>
                 <h1>Pool cleaners R us</h1>
@@ -41,6 +45,7 @@ export default function Portfolio() {
                 <div>
                     <p>Game using HTML canvas. I used Class components for my user character and mapped out the stage with an array so that multimaps can be loaded in later.</p>
                 </div>
+                <Link target="_blank" href="https://github.com/PvrkFvmily/project-1-Pool-Cleaners-R-Us"><h3>Check it out on GitHub!</h3></Link>
             </div>
             <style jsx>{`
                 p {
